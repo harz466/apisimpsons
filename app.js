f6bb17c5d5601a923fdf3b7f6267b9797128dbf1
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-12-06 15:49:03 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
+
+const main = document.getElementById("main");
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => {
+    createCard(data[0]);
+  })
+  .catch((err) => console.error(err));
+
+function createCard(data) {
+  const { character, quote, image } = data;
+
+  const card = document.createElement("div");
+  const title = document.createElement("h2");
+  const img = document.createElement("img");
+  const quoteCharacter = document.createElement("p");
+  const button = document.createElement("button");
+
+  title.textContent = character;
+  img.src = image;
+  img.alt = image;
+  quoteCharacter.textContent = quote;
+  button.textContent = "Generate";
+
+  card.classList.add("card");
+  title.classList.add("title");
+  img.classList.add("img");
+  quoteCharacter.classList.add("quoteCharacter");
+  button.classList.add("button");
+
+  main.appendChild(card);
+  card.appendChild(title);
+  card.appendChild(img);
+  card.appendChild(quoteCharacter);
+  card.appendChild(button);
+
+  button.addEventListener("click", () => {
+    window.location.reload();
+  });
+}
